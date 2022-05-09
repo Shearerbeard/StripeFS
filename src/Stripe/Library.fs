ba72197fde951da89de5b97c84c765dee0c6f5f0
Mike Shearer <shearerbeard@gmail.com>
2022-05-09 11:38:16 -0600

    fixes paket deps by adding FSharp.Core as a dep https://github.com/fsprojects/Paket/issues/3972\#issue-774752633

--- src/Stripe/Library.fs
+++ src/Stripe/Library.fs
@@ -1,4 +1,17 @@
-﻿namespace Stripe
+﻿namespace StripeFS
+
+namespace StripeFS.Types
+
+type Undefined = exn
+
+namespace StripeFS
+open StripeFS.Types
+module Customer =
+    open Stripe
+    let createCustomer name  = Undefined
+
+module Subscription =
+    let createSubscription = Undefined
 
 module Say =
     let hello name =
