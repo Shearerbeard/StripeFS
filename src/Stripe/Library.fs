8fc87dc422d4ffa9fe4831f20e50e267192fa867
Mike Shearer <shearerbeard@gmail.com>
2022-05-09 12:32:38 -0600

    base implementation for customer and subscription create

--- src/Stripe/Library.fs
+++ src/Stripe/Library.fs
@@ -1,17 +1,48 @@
-﻿namespace StripeFS
-
-namespace StripeFS.Types
+﻿namespace StripeFS.Types
 
 type Undefined = exn
 
 namespace StripeFS
+
 open StripeFS.Types
+open Stripe
+
+module Util =
+    let tryCatch f x =
+        try
+            f x |> Ok
+        with
+        | ex -> Error ex.Message
+
+module Config =
+    let setApiKey keyStr = Stripe.StripeConfiguration.ApiKey = keyStr
+
 module Customer =
-    open Stripe
-    let createCustomer name  = Undefined
+    open Util
+    let createCustomer name email =
+        let shipping = ShippingOptions(
+            Address = AddressOptions(
+                Line1 = "10421 W Skycrest Dr",
+                City = "Boise",
+                State = "ID",
+                PostalCode = "83704",
+                Country = "US"))
+
+        CustomerCreateOptions(
+            Name = name,
+            Email = email,
+            Shipping = shipping)
+        |> (tryCatch <| CustomerService().Create)
 
 module Subscription =
-    let createSubscription = Undefined
+    open Util
+    let createSubscription customerId priceId =
+        let subscriptions =
+            ResizeArray<SubscriptionItemOptions> [SubscriptionItemOptions(Price = priceId)]
+
+        SubscriptionCreateOptions(Customer = customerId, Items = subscriptions)
+        |> (tryCatch <| SubscriptionService().Create)
+
 
 module Say =
     let hello name =
